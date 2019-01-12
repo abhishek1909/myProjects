68f3e1973191044bd6a7a8238efc41d60ef692ad
Abhishek <abhishek20031995@gmail.com>
2019-01-12 22:55:02 +0530

    Add Comments to java and python code

--- javaPractices/printHello.java
+++ javaPractices/printHello.java
@@ -1,5 +1,6 @@
 public class printHello{
     public static void main(String[] args){
+	//Print Hello from JAVA
         System.out.println("Hello Java");
     }
 }
--- pythonPractices/printHello.py
+++ pythonPractices/printHello.py
@@ -1,1 +1,2 @@
+# Print Hello from Python
 print("Hello Python")
