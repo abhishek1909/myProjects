f238b70f2334f0cccd167a292918b0bfd7dcd094
Abhishek <abhishek20031995@gmail.com>
2019-01-20 13:50:25 +0530

    commit 2

--- javaPractices/printHello.java
+++ javaPractices/printHello.java
@@ -2,6 +2,7 @@ public class printHello{
     public static void main(String[] args){
 	//Print Hello from JAVA
         System.out.println("Hello Java");
-	// Commit 1			   
+	// Commit 1
+	// Commit 2
     }
 }
